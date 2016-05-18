f1cd62b1efc1c2595d58ef575a59a6536564acc8
Wyatt Giles <wyatt.giles@suusuccess.org>
2016-05-18 09:45:45 -0600

    added more doe to chose what you want to do

--- Renter Tracker.cpp
+++ Renter Tracker.cpp
@@ -2,8 +2,34 @@
 //By Wyatt Giles, Brandan Ivie, Tanner Davis
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include <fstream>
+#include <ctime>
 using namespace std;
+void read ();
+void write ();
+void newwrite ();
 int main(){
-	
+	string what = "";
+	string file = "";
+	cout << "'stop' will end the program." << endl << endl;
+	while(what != "stop"){
+		cout << "What would you like to do?" << endl << 
+		getline (cin, what);
+		if (what == "add"){
+//			write();
+		}	
+		else if (what == "new"){
+//			write();
+		}
+		else if (what == "read"){
+			cout << "What file do you want to read?" << endl; 		
+		}
+		else if (what == "stop"){
+			return 0;
+		}
+		else{
+			cout << "!Not an option!" << endl;
+		}
+	}
 }
