d025d294ac4828a41439ccdeb9a52df2867589fc
Wyatt Giles <wyatt.giles@suusuccess.org>
2016-05-23 09:10:31 -0600

    changed a lot in code
    added edit function but did not get it to work
    added a Text change to not rewrite did not work

--- Renter Tracker.cpp
+++ Renter Tracker.cpp
@@ -12,14 +12,16 @@ using namespace std;
 void read ();
 void write ();
 void sortFile();
+void edit();
 int main(){
 	int menuOption = 0;
   	int exitVar = 1;
 	do {
-		//display menu and get option
+		//display menu and get options
 		cout << endl;
 		cout << "1 Read the file" << endl;
-		cout << "2 Create a new file" << endl;
+		cout << "2 Create an item" << endl;
+//		cout << "3 Edit file" << endl;
 		cout << "3 End Program " << endl;
 		cout << "Enter menu option: ";
 		cin >> menuOption;
@@ -33,6 +35,10 @@ int main(){
 			case 2:
 				write();
 				break;
+/*			case 4:
+				edit();
+				break; 
+*/
 			case 3:
 				exitVar = 0;
 				break;
@@ -42,7 +48,7 @@ int main(){
 		}
 
 	} while (exitVar != 0);
-	sortFile();
+//	sortFile();
 	return 0;
 }//end main function
 void read (){
@@ -71,11 +77,13 @@ void read (){
 
 void write (){
 	string filename = "";
+	string filecontents = "";
+	bool x = true;
 	string name = ""; // name of object
 	string user = ""; // name of person
 	string addr = ""; // address
 	string date = ""; // when it will be back
-	cout << "What is the new file's name?" << endl;
+	cout << "What is the file's name?" << endl;
 	cin >> filename;
 	fstream file;
 	file.open(filename, ios::app);
@@ -93,9 +101,57 @@ void write (){
 	fstream textfile;
 	textfile.open("Text", ios::app);
 	textfile << filename << endl;
+//checks to see if filename is already written-can also use new function to write to a file
+/*	while(!textfile.eof()){
+		getline (textfile, filecontents);
+		if(filecontents == filename){
+			x = false;
+		}
+		if(x == true){
+			textfile << filename << endl;
+		}
+	}
+*/
 	textfile.close();
-	
+}
+/*
+void edit(){
+	string file = "";
+	string files = "";
+	string item = "";
+	string read = "";
+	cout << "What file do you want to edit?" << endl << endl;
+	ifstream File ("Text");
+	if (File.is_open()){
+		while(!File.eof()){
+			getline (File, files);
+			cout << files << endl;
+		}
+	}
+	File.close();
+	cin >> file;
+	cout << endl;
+	ifstream editfile (file);
+	if (editfile.is_open()){
+		while(!editfile.eof()){
+			getline (editfile, files);
+			cout << files << endl;
+		}
+	}
+	// below this is what needs to be fixed for finding where the object is
+	cout << "What item do you want to edit?";
+	cin >> item;
+	cout << endl;
+	ifstream editfile (file);
+	if (editfile.is_open()){
+		while(!editfile.eof()){
+			getline (editfile, item, "-------------------");
+			cout << item << endl;
+		}
+	}
 }
 void sortFile(){
+// will need to sort each file
 	
 }
+*/
